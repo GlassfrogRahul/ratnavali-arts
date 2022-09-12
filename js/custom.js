f07003953695af30759918139b078148a2f491c8
rahul2cool82 <write2ranarahul@gmail.com>
2022-09-12 17:59:02 +0530

    first commit

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+function playVideo(btn) {
+    const videoElement = btn.parentElement.querySelector('video');
+    videoElement.play();
+    btn.parentElement.querySelector('.pause').classList.remove('active');
+    btn.parentElement.querySelector('.play').classList.add('active');
+}
+
+function pauseVideo(btn) {
+    const videoElement = btn.parentElement.querySelector('video');
+    videoElement.pause();
+    btn.parentElement.querySelector('.play').classList.remove('active');
+    btn.parentElement.querySelector('.pause').classList.add('active');
+}
